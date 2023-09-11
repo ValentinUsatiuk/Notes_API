4a9442a5f0ad6f6afbfb84e7f1bae8c2d88d1322
valentin_usatiuk <117016618+ValentinUsatiuk@users.noreply.github.com>
2023-09-11 23:53:55 +0300

    Create methods for notes

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-
 
 app = Flask(__name__)
 
@@ -11,8 +10,12 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
 # Ініціалізація об'єкта SQLAlchemy
 db = SQLAlchemy(app)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255))
+    content = db.Column(db.Text)
+    created_on = db.Column(db.Date)
 
 
 # Документація API
@@ -22,23 +25,68 @@ def home():
 
 
 # Отримуємо список усіх заміток
-@app.route("/notes", methods=["GET"])
+@app.route("/notes", methods=["GET", "POST"])
 def all_notes():
-    result_dict = {}
-    return result_dict
-
+    if request.method == "GET":
+        all_notes = Note.query.all()
+        notes_list = []
+        for note in all_notes:
+            note_data = {
+                "id": note.id,
+                "title": note.title,
+                "content": note.content,
+                "created_on": note.created_on.strftime('%Y-%m-%d %H:%M:%S'),
+            }
+            notes_list.append(note_data)
+        return jsonify(notes=notes_list)
+    if request.method == "POST":
+        data = request.get_json()
+        if "title" not in data or "content" not in data:
+            return jsonify({"error": 'Insufficient data to create note'})
+        new_note = Note(
+            title=data["title"],
+            content=data["content"],
+            created_on=datetime.utcnow(),
+        )
+        db.session.add(new_note)
+        db.session.commit()
+        return jsonify({'message': 'The note was created successfully'})
 
 # Отримуємо конкретну замітку за id/ новлення існуючої замітки за id/
 # Видалення замітки за id
 @app.route("/notes/<int:note_id>", methods=["GET", "PUT", "DELETE"])
 def note(note_id):
-    result_dict = {}
-    return result_dict
+    note = Note.query.get(note_id)
+    if note:
+        if request.method == "GET":
+            result_dict = {
+                "id": note.id,
+                "title": note.title,
+                "content": note.content,
+                "created_on": note.created_on.strftime('%Y-%m-%d %H:%M:%S'),
+            }
+            return result_dict
+        if request.method == "PUT":
+            data = request.get_json()
+            if note is None:
+                return jsonify({'error': "This note doesn't exist"}), 404
+            if "title: in data":
+                note.title = data["title"]
+            if "content" in data:
+                note.content = data["content"]
+            db.session.commit()
+            return jsonify({"message": "Note updated successfully"})
+        if request.method == "DELETE":
+            if note is None:
+                return jsonify({'error': "This note doesn't exist"}), 404
+            db.session.delete(note)
+            db.session.commit()
+            return jsonify({"message": "Note delete successfully"})
+    else:
+        return jsonify({"error": "This note doesn't exist"}, 404)
 
 
-# Створення нової замітки
-@app.route("/methods", methods=["POST"])
-def new_notes(title, context):
-    result_dict = {}
-    return result_dict
-
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+        app.run(debug=True)
